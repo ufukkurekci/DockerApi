13ceccea3eda536c97500907abea6a9b104c3e5c
ufukkurekcii <ufukkurekci@hotmail.com>
2023-04-04 01:52:08 +0300

    port 1000

--- DockerApi/Program.cs
+++ DockerApi/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// default port :1000
 builder.WebHost.ConfigureKestrel(options =>
 {
 	options.ListenAnyIP(1000);
